08ee192c0047d89322aff9f42465159e3fa89fde
windeaker@arch <windeaker@foxmail.com>
2019-11-25 15:52:06 +0800

    push

---
+++ src/main/java/info/windeaker/thread/lock/ReentranLockTest.java
@@ -0,0 +1,7 @@
+package info.windeaker.thread.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentranLockTest {
+    private final static ReentrantLock lock=new ReentrantLock();
+}
